ddb716f1dd750ac3e4f7a90a51affc13b8be3ff8
JimLGray <jim.lyndon.gray@gmail.com>
2018-03-26 22:40:26 +1100

    added body of some functions in scoring.py

--- scoring.py
+++ scoring.py
@@ -3,22 +3,36 @@ Scoring functiongs will go in this file. They probably will involve sparse
 matrices
 '''
 
+import numpy as np
+from model import UserInput
 
-def area():
+
+def area(user_input):
     ''' Calculates the enclosed area of a border. This is going to have to count
     the number of enclosed points. Assumes that all borders are enclosed.
     '''
-    pass
+    return np.sum(user_input) #returns number of true elements
 
 
-def compare_matrix():
+def compare_matrix(user_input, country):
     ''' Compares 2 arrays of same size and returns sum/list of different
     elements.
     '''
+
+    diff = np.logical_xor(user_input, country)
+    similar = np.logical_and(user_input, country)
+
     pass
 
 
 def perimiter():
     ''' Calculates number of edge elements in matrix
+    '''
+    pass
+
+
+def most_similar(user_input, map_list):
+    '''
+    goes through a loop of countries and compares them???
     '''
     pass
