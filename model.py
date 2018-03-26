a0db7019443b54148924266ad2edcc19dd090828
Owen Cassidy <owen.cassidy@gmail.com>
2018-03-26 22:35:07 +1100

    Changed model to numpy array

--- model.py
+++ model.py
@@ -1,12 +1,8 @@
 from scipy.sparse import coo_matrix
+import numpy as np
 
 class UserInput:
-    __data = []
-    def __init__(self):
-        for x in range(0,1280):
-            self.__data.append([])
-            for y in range(0,720):
-                self.__data[x].append(False)
+    __data = np.zeros((1280, 720), dtype=bool)
 
     def draw(self, x, y):
         self.__data[x][y] = True
@@ -15,6 +11,4 @@ class UserInput:
         return self.__data
 
     def __str__(self):
-        data = coo_matrix(self.__data)
-        return str(data.toarray())
-        # just because it prints nicely
+        return str(self.__data)
